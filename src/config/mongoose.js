489890c287d76ef4f3035fb82039a9cf01f6c1d8
zohaib5424 <zohaib5424@gmail.com>
2024-03-11 01:16:49 +0500

    code clean

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -3,9 +3,8 @@ import dotenv from 'dotenv';
 
 // Load and configure dotenv
 dotenv.config();
-console.log('mongo========');
-console.log('process.env.MONGO_DB_HOST: ', process.env.MONGO_DB_URL);
-const mongo_url = process.env.MONGO_DB_URL; //`mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB}?maxPoolSize=${process.env.MONGO_POOL_SIZE}`;
+
+const mongo_url = process.env.MONGO_DB_URL;
 
 console.log('mongo_url: ', mongo_url);
 
--- src/models/booking.js
+++ src/models/booking.js
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-
-// import { v1 as uuidv1 } from 'uuid';
-
-// /**
-//  * Booking Schema
-//  */
-// const bookingSchema = new mongoose.Schema({
-//   bookingId: { type: String, default: _ => uuidv1() },
-//   hotelName: { type: String, required: true },
-//   guestName: { type: String, required: true },
-//   message: { type: String, required: true },
-
-//   checkInDate: { type: Date },
-//   checkOutDate: { type: Date },
-// });
-
-// export default mongoose.model('Booking', bookingSchema);
-
 import mongoose from 'mongoose';
 const bookingSchema = new mongoose.Schema(
   {
